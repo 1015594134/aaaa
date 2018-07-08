655348775a6d8a2aa287afd687d4cd6506027142
1015594134 <1015594134+1015594134@qq.github.com>
2018-07-08 09:33:59 +0800

    这是aaaa项目

---
+++ A.java
@@ -0,0 +1,25 @@
+public class A {
+    private int a;
+    private String b;
+
+    public A() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+
+}
